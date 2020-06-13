961fda85b6a7c1174fab6bde96cdf738cc1c0cf9
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-06-13 16:17:21 +0200

    Réarrangements du code

--- docker/src/musician.js
+++ docker/src/musician.js
@@ -3,26 +3,23 @@
 // Standard Node module to work with UDP
 const dgram = require('dgram');
 
+// Import protocol
+const protocol = require('./protocol.js');
+
 // To create RFC4122 compliant UUIDs
 // FROM: https://github.com/uuidjs/uuid
 const uuid = require('uuid');
 const uuidv4 = uuid.v4();
 
-//Creating a datagram socket
+// Creating a datagram socket
 const server = dgram.createSocket('udp4');
 
-var instruments = new Map();
-instruments.set("piano", "ti-ta-ti");
-instruments.set("trumpet", "pouet");
-instruments.set("flute", "trulu");
-instruments.set("violin", "gzi-gzi");
-instruments.set("drum", "boum-boum");
 
 console.log("Starting musician...")
 var instrument = process.argv[2];
 
-if(instruments.has(instrument)) {
-    setInterval(play, 1000);
+if(protocol.instruments.has(instrument)) {
+    setInterval(play, protocol.playTimer);
 } else {
     console.log("Unknown instrument!")
 }
@@ -30,13 +27,13 @@ if(instruments.has(instrument)) {
 function play() {
     var musician = {
         uuid: uuidv4,
-        sound: instruments.get(instrument)
+        sound: protocol.instruments.get(instrument)
     };
 
     const payload = JSON.stringify(musician);
 
     message = new Buffer.from(payload);
-    server.send(message, 2112, "239.255.22.5", function() {
+    server.send(message, protocol.port, protocol.multicast_address, function() {
         console.log(payload);
     });
 }
---
+++ docker/src/protocol.js
@@ -0,0 +1,11 @@
+exports.port = 2112;
+exports.multicast_address = "239.255.22.5";
+exports.playTimer = 1000;
+
+// to map instruments with their sounds
+exports.instruments = new Map();
+exports.instruments.set("piano", "ti-ta-ti");
+exports.instruments.set("trumpet", "pouet");
+exports.instruments.set("flute", "trulu");
+exports.instruments.set("violin", "gzi-gzi");
+exports.instruments.set("drum", "boum-boum");
